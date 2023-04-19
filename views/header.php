6f5e33b0d610792732f836bbf466a3665d5bb35c
basmalagamal <125498490+basmalagamal@users.noreply.github.com>
2023-04-19 14:13:08 +0200

    manual commit
    to be merged into master

---
+++ views/header.php
@@ -0,0 +1,20 @@
+<?php
+
+
+$server = "localhost";
+$username = "root";
+$password = " ";
+$dbname = "movieworld";
+
+$conn = mysqli_connect($server, $username, $password, $dbname);
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+       <meta charset="UTF-8">
+       <title> Movie Search</title>
+       <link rel="stylesheet" href="main.css">
+    </head>
+    <body>
